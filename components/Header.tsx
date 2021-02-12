71b3d0eb26e9a6c4919cf00c89fcaebc6c987f24
Byeong Hoon <byeonghooni@icloud.com>
2021-02-12 17:00:19 +0900

    feat(Header) add Header Component

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../styles/palette';
+
+const Container = styled.div`
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 52px;
+	padding: 0 12px;
+	border-bottom: 1px solid ${palette.gray};
+	h1 {
+		font-size: 21px;
+	}
+`;
+
+const Header: React.FC = () => (
+	<Container>
+		<h1>Jerrynim&amp;apos;s TodoList </h1>
+	</Container>
+);
+
+export default Header;
