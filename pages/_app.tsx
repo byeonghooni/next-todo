62a82b97c18404c58383cbb994800ce256d05674
Byeong Hoon <byeonghooni@icloud.com>
2021-02-13 17:03:14 +0900

    feat(Footer) add Footer Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { AppProps } from 'next/app';
 import GlobalStyle from './GlobalStyle';
+
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const app = ({ Component, pageProps }: AppProps) => (
 	<>
 		<GlobalStyle />
 		<Header />
 		<Component {...pageProps} />
+		<Footer />
 	</>
 );
 
